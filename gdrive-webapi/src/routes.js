667c76b90872a7613bf6da5000ec0303aeb7c30e
Gabriel Amorim <therealamorim@gmail.com>
2021-12-23 11:01:21 -0300

    second class of javascript expert week 05

--- gdrive-webapi/src/routes.js
+++ gdrive-webapi/src/routes.js
@@ -1,12 +1,14 @@
-import FileHelper from "./fileHelper.js";
-import { logger } from "./logger.js";
 import { dirname, resolve } from 'path';
-import { fileURLToPath } from 'url';
+import { pipeline } from 'stream/promises';
+import { fileURLToPath, parse } from 'url';
+import FileHelper from './fileHelper.js';
+import { logger } from './logger.js';
+import UploadHandler from './uploadHandler.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const defaultDownloadsFolder = resolve(__dirname, '../', 'downloads');
 
-export default class Routes {
+class Routes {
     io;
 
     constructor(downloadsFolder = defaultDownloadsFolder) {
@@ -28,8 +30,30 @@ export default class Routes {
     }
 
     async post(request, response) {
-        logger.info('post');
-        response.end();
+        const { headers } = request;
+
+        const { query: { socketId } } = parse(request.url, true);
+
+        const uploadHandler = new UploadHandler({
+            socketId,
+            io: this.io,
+            downloadsFolder: this.downloadsFolder,
+        });
+
+        const onFinish = (response) => () => {
+            response.writeHead(200);
+            const data = JSON.stringify({ result: 'Files uploaded with success!' });
+            response.end(data);
+        } 
+        
+        const busboyInstance = uploadHandler.registerEvents(headers, onFinish(response));
+
+        await pipeline(
+            request,
+            busboyInstance,
+        );
+
+        logger.info('Request finished with success!!');
     }
 
     async get(request, response) {
@@ -46,3 +70,5 @@ export default class Routes {
         return chosen.apply(this, [request, response]);
     }
 }
+
+export { Routes };
